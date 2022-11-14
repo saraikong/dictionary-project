1151b823bb1ff4ab2f338725f650f55bc4fba9b5
saraikong <94483250+saraikong@users.noreply.github.com>
2022-11-13 23:47:39 -0600

    Created phonetics component

--- src/Results.js
+++ src/Results.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetics from "./Phonetics";
 
 export default function Results(props) {
   console.log(props.results);
@@ -7,6 +8,13 @@ export default function Results(props) {
     return (
       <div>
         <h2>{props.results.word}</h2>
+        {props.results.phonetics.map(function (phonetics, index) {
+          return (
+            <div key={index}>
+              <Phonetics phonetics={phonetics} />
+            </div>
+          );
+        })}
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
