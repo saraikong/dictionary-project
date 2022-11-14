c45503aa951622f01f5d7bdc3c468b181aea1cb6
saraikong <94483250+saraikong@users.noreply.github.com>
2022-11-13 21:32:37 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import logo from "./logo.png";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <img src={logo} alt="logo" className="App-logo img-fluid" />
+    </div>
+  );
+}
+
+export default App;
