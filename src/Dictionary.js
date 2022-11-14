4fbc97fed6a716eba4f25c94ea281256a3df77aa
saraikong <94483250+saraikong@users.noreply.github.com>
2022-11-13 23:16:43 -0600

    Centered the search bar

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -26,7 +26,11 @@ export default function Dictionary() {
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
-        <input type="search" onChange={handleKeywordChange} />
+        <input
+          type="search"
+          onChange={handleKeywordChange}
+          placeholder="Enter a word"
+        />
       </form>
       <Results results={results} />
     </div>
