c160f0b51be3bcf1c91161cf091a3f61cfad4e47
saraikong <94483250+saraikong@users.noreply.github.com>
2022-11-14 23:16:14 -0600

    updated footer

--- src/App.js
+++ src/App.js
@@ -12,7 +12,24 @@ function App() {
           </h1>
         </header>
         <Dictionary defaultKeyword={randomWords(5)} />
-        <footer>Coded by Sarai Kong</footer>
+        <footer>
+          This project was coded by Sarai Kong and is{" "}
+          <a
+            href="https://github.com/saraikong/dictionary-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://gentle-clafoutis-9c90c4.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
       </div>
     </div>
   );
